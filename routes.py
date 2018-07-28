de26718ed6b474d6a5828c76adca80f52b0a6aee
Raymond Truong <raymond.truong@hotmail.ca>
2018-07-27 22:50:53 -0400

    Add basic edge detection

--- routes.py
+++ routes.py
@@ -1,15 +1,40 @@
 from app import app
 from flask import render_template, request
 import json
+import matplotlib.pyplot as plt
+import base64
+from PIL import Image
+from io import BytesIO
+import numpy as np
+import cv2
+
+
 
 @app.route("/")
 @app.route("/index")
 def index():
     return render_template("index.html", test="Home")
 
+
 @app.route("/solve", methods=["POST"])
 def solve():
     test = "Test"
     json_images = request.form["images"]
     images = json.loads(json_images)
-    return render_template("solve.html", test=len(images), images=images)
+    new_images = [None] * 6
+
+    for i in range(6):
+        img = base64.b64decode(images[0][21:])
+        npimg = np.fromstring(img, dtype=np.uint8)
+
+        source = cv2.imdecode(npimg, 1)
+        edges = cv2.Canny(source,100,200)
+
+        (_, x) = cv2.imencode(".png", edges)
+        y = "data:image/png;base64," + str(base64.b64encode(x))[2:-1]
+
+
+        # cv2.imshow("some window", edges)
+        new_images[i] = y
+
+    return render_template("solve.html", test=y, old_images=images, new_images=new_images)
