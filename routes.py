1a37ad49691439589fa554aea0dbf232e0a52522
Raymond Truong <raymond.truong@hotmail.ca>
2018-07-31 15:29:10 -0400

    Improve image capturing process

--- routes.py
+++ routes.py
@@ -28,9 +28,13 @@ def solve():
         npimg = np.fromstring(img, dtype=np.uint8)
 
         source = cv2.imdecode(npimg, 1)
-        edges = cv2.Canny(source,100,200)
+        crop_source = source[18:160, 46:188] # 142x142
 
-        (_, x) = cv2.imencode(".png", edges)
+        edges = cv2.Canny(crop_source,100,200)
+        # im2, contours, hierarchy = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+        # new = cv2.drawContours(edges, contours, -1, (0,255,0), 3)
+
+        ret, x = cv2.imencode(".png", edges)
         y = "data:image/png;base64," + str(base64.b64encode(x))[2:-1]
 
 
