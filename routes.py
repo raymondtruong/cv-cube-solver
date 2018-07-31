2fb64565a02edecbe0553111355ed789cd8dd86c
Raymond Truong <raymond.truong@hotmail.ca>
2018-07-31 17:40:24 -0400

    Prepare colour recognition

--- routes.py
+++ routes.py
@@ -7,7 +7,8 @@ from PIL import Image
 from io import BytesIO
 import numpy as np
 import cv2
-
+import colorsys
+import sys
 
 
 @app.route("/")
@@ -18,27 +19,47 @@ def index():
 
 @app.route("/solve", methods=["POST"])
 def solve():
-    test = "Test"
-    json_images = request.form["images"]
-    images = json.loads(json_images)
-    new_images = [None] * 6
+
+    b64_raw_images = json.loads(request.form["images"])
+    b64_squares = [[[None, None, None], [None, None, None], [None, None, None]],
+                  [[None, None, None], [None, None, None], [None, None, None]],
+                  [[None, None, None], [None, None, None], [None, None, None]],
+                  [[None, None, None], [None, None, None], [None, None, None]],
+                  [[None, None, None], [None, None, None], [None, None, None]],
+                  [[None, None, None], [None, None, None], [None, None, None]]]
 
     for i in range(6):
-        img = base64.b64decode(images[0][21:])
-        npimg = np.fromstring(img, dtype=np.uint8)
+        # decode image
+        encoded_image = np.fromstring(base64.b64decode(b64_raw_images[i][21:]), dtype=np.uint8)
+        source = cv2.imdecode(encoded_image, 1)
 
-        source = cv2.imdecode(npimg, 1)
-        crop_source = source[18:160, 46:188] # 142x142
+        # crop
+        cropped_source = source[17:161, 45:189]     # 144x144 crop
 
-        edges = cv2.Canny(crop_source,100,200)
-        # im2, contours, hierarchy = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-        # new = cv2.drawContours(edges, contours, -1, (0,255,0), 3)
+        # individual squares
+        for row in range(3):
+            for column in range(3):
 
-        ret, x = cv2.imencode(".png", edges)
-        y = "data:image/png;base64," + str(base64.b64encode(x))[2:-1]
+                # slice square
+                square = cropped_source[(row * 48):((row + 1) * 48), (column * 48):((column + 1) * 48)]
+
+                # extract colour
+                colour = square[24, 24]
+                rgb = (colour[2], colour[1], colour[0])
+                print("{} = {}".format(i, identify_colour(rgb)), file=sys.stderr)
+
+                # encode square
+                ret, x = cv2.imencode(".png", square)
+                b64_squares[i][row][column] = "data:image/png;base64," + str(base64.b64encode(x))[2:-1]
 
 
-        # cv2.imshow("some window", edges)
-        new_images[i] = y
+    return render_template("solve.html", test="Test", squares=b64_squares)
 
-    return render_template("solve.html", test=y, old_images=images, new_images=new_images)
+
+def identify_colour(rgb):
+    # todo
+    hex = "#%02x%02x%02x" % rgb
+    hsv = colorsys.rgb_to_hsv(rgb[0], rgb[1], rgb[2])
+    print(hsv, file=sys.stderr)
+
+    return hex
